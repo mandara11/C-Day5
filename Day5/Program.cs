4c88b2d7100e76962f6c929efbc064354571e466
mandara11 <mandaram2345@gmail.com>
2022-04-23 10:10:41 +0530

    Add project files.

---
+++ Day5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            //EvenOdd.evenodd();
+         // Leapyear.checkyearinput();
+          //Leapyear.Yearmethod();
+            Flipcoin.FindFlipcoin();
+            Console.ReadKey();
+        }
+    }
+}
